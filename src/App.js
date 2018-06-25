7952e4a1cee56c4641189f653670ea8cb920dd25
Maxim Sukharev <maxim@sourced.tech>
2018-06-25 17:49:39 +0200

    read filter results in js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import { ProtocolServiceClient } from './_proto/protocol_pb_service';
 import { ParseRequest } from './_proto/protocol_pb';
+import { Node } from './_proto/uast_pb';
 import './App.css';
 
 /* global Module */
+function readArray(ptr, length) {
+  let result = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    result[i] = Module.HEAP8[ptr + i];
+  }
+  return result;
+}
+
 function callLibuast(uast, query) {
   return new Promise((resolve, reject) => {
     const api = {
-      filter: Module.cwrap('filter', 'number', ['array', 'number', 'string'])
+      filter: Module.cwrap('filter', 'number', ['array', 'number', 'string']),
+      getNodesSize: Module.cwrap('get_nodes_size', 'number', ['number']),
+      getNodeSize: Module.cwrap('get_node_size', 'number', [
+        'number',
+        'number'
+      ]),
+      getNode: Module.cwrap('get_node', 'number', ['number']),
+      free: Module.cwrap('free_filter', 'number', [])
     };
     console.log('call:');
-    const r = api.filter(uast, uast.length, query);
-    resolve(r);
+    const resultPointer = api.filter(uast, uast.length, query);
+    console.log('resultPointer', resultPointer);
+    const nodes = api.getNodesSize(resultPointer);
+    console.log('getNodesSize', nodes);
+
+    const result = [];
+
+    for (let i = 0; i < nodes; i++) {
+      const nodeSize = api.getNodeSize(resultPointer, 1);
+      console.log('getNodeSize', nodeSize);
+      const node = api.getNode(resultPointer, 1);
+      console.log('getNode', node);
+
+      const arr = readArray(node, nodeSize);
+      const n = Node.deserializeBinary(arr);
+      //console.log('n', n);
+
+      result.push(n.toObject());
+    }
+
+    api.free(resultPointer);
+
+    resolve(result);
   });
 }
 
@@ -105,7 +142,11 @@ class App extends Component {
           <button onClick={this.handleFilter} disabled={!wasmReady}>
             Filter
           </button>
-          <div>Nodes found: {filterResult}</div>
+          {filterResult ? (
+            <div>
+              Nodes found: <pre>{JSON.stringify(filterResult, null, '  ')}</pre>
+            </div>
+          ) : null}
           <br />
         </div>
         <div>
