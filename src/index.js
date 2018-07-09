a4b460f0ac55601a7026f7106a1bd67b8180209d
Maxim Sukharev <maxim@sourced.tech>
2018-07-09 17:17:02 +0200

    export Node

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Client from './client';
 import { init, protoToMap } from './libuast';
 import { roleToString } from './utils';
+import { Node } from './_proto/uast_pb';
 
 export default Client;
 
-export { protoToMap, init as initLibuast, roleToString };
+export { protoToMap, init as initLibuast, Node, roleToString };
